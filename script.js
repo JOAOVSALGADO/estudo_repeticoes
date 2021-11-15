6721e875ead3f3ad7a4efbbe18a03b2490d9514d
Joao V. Salgado <joaovsalgado22@gmail.com>
2021-11-15 11:37:23 -0300

    Construção de repeticoes com "do" e "while".

---
+++ script.js
@@ -0,0 +1,13 @@
+// var c = 1 
+//while(c<50){
+//console.log('tudo bem' + c)
+//c++
+//}
+
+var c = 1
+//Os dois programas tem a mesma funcionalidade, o "do" quer dizer que ira imprimir determinado comando enquanto "while" satistisfazer essa condição.
+do {
+    console.log('tudo bem ' + c)
+    c++
+}
+while (c < 10) 
