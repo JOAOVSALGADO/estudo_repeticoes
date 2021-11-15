71fd570330e6fa9a09bfa76c8b2647b7a44ee29b
Joao V. Salgado <joaovsalgado22@gmail.com>
2021-11-15 14:03:01 -0300

    Configuração de mudar a background de acordo com cada resultador

--- script.js
+++ script.js
@@ -31,17 +31,23 @@ function verificar() {
     var nu2 = Number(num2.value)
     var nu3 = Number(num3.value)
     res.innerHTML = "Contando: "
+    
     if (num1.value.length ==0 || num2.value.length == 0 || num3.value.length == 0){window.alert("[ERRO] faltam dados")}
     else{if (nu1<nu2){
         for (var c = nu1; c <= nu2; c += nu3) {
-            res.innerHTML += ' ' + c
+            res.innerHTML += ' ' + c + '\u{1f449}'
+            document.body.style.background = '#515154' 
         }
     
     }else {
         for (var c = nu1; c >= nu2; c -= nu3) {
-            res.innerHTML += ' ' + c
+            res.innerHTML += ' ' + c + '\u{1f449}'
+
+            document.body.style.background = 'blue' 
+            
     }
-        
-}}
+      
+}res.innerHTML += "\u{1f3c1}" 
+}
     
 }
